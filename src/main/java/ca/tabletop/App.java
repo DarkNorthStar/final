896abad17fa5ee061ba6d9baded32ed8874b36ad
DarkNorthStar <mr_tyler09@live.ca>
2023-04-04 14:16:09 -0400

    Format App

--- src/main/java/ca/tabletop/App.java
+++ src/main/java/ca/tabletop/App.java
@@ -1,5 +1,6 @@
 package ca.tabletop;
 
+// Imports
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -22,8 +23,9 @@ public class App extends Application
     //                  CONSTANTS
     //****************************************************/
     // Server Constants
-    private static final int DEFAULT_PORT = 5969;
+    private static final int DEFAULT_PORT = 5969; // Port used for the server
     // Command Constants
+    // Constants for all commands sent by the game master instance
     private static final String MUSIC_NEXT_COMMAND = "?music-next";
     private static final String MUSIC_PREVIOUS_COMMAND = "?music-previous";
     private static final String MUSIC_TOGGLE_COMMAND = "?music-toggle";
@@ -41,28 +43,28 @@ public class App extends Application
     private static List<File> musicFiles = FileManager.loadMusicFiles(); // music
     private static List<File> effectFiles = FileManager.loadEffectFiles(); // effect
     private static List<File> imageFiles = FileManager.loadImageFiles(); // images
-    // Server Client Variables
+    // Server / Client Variables
     private static Server server; // Server for game master instances
-    //private static int numberOfPlayers = 0;
     private static Client client; // Client for game player instances
 
     // Functions called when game master is launched
     public static void startGameMaster() throws IOException, UnsupportedAudioFileException, LineUnavailableException
     {
-        // Starts the server for the game master
+        // Starts the server for the game master to send messages to connected instances of game players
         server = new Server(DEFAULT_PORT);
-        // Loads the music and effect files into the audio player
+        // Loads the music and effect files into the audio player to be played
         AudioPlayer.readyAudioPlayer(musicFiles, effectFiles);
-
+        // Loads images into the slide show to be displayed
         SlideShow.readyImages(imageFiles);
     }
+    // Function called when the game player is launched. used to load the data needed for the game player
     public static void startGamePlayer(GamePlayerController controller) throws UnsupportedAudioFileException, IOException, LineUnavailableException
     {
-        // Loads the music and effect files into the audio player
+        // Loads the music and effect files into the audio player to be played
         AudioPlayer.readyAudioPlayer(musicFiles, effectFiles);
-
+        // Loads the image files into the slide show to be displayed
         SlideShow.readyImages(imageFiles);
-
+        // Starts the command reader thread to wait for commands comming in from the game master
         CommandReader commandReader = new CommandReader(controller);
         commandReader.start();
     }
@@ -85,30 +87,35 @@ public class App extends Application
     //****************************************************/
     //                  CLIENT CONTROLS
     //****************************************************/
+    // Connects to a server using the passed ip and DEFAULT_PORT
     public static boolean connectToServer(String ip)
     {
+        // Creates a empty client
         client = new Client();
+        // Connects the client to the server using the ip and port
         return client.connectToServer(ip, DEFAULT_PORT);
     }
     //****************************************************/
     //                  FXML FUNCTIONS
     //****************************************************/
+    // Startup  function
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException 
+    {
         scene = new Scene(loadFXML("mainMenu"), 1280, 720);
         stage.setScene(scene);
         stage.show();
     }
-
+    // Sets the root fxml document of the main window
     public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
-
+    // Loads FXML documents
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
-
+    // Main
     public static void main(String[] args) {
         launch();
     }
@@ -122,7 +129,7 @@ public class App extends Application
     public static List<File> getEffectFiles(){return effectFiles;}
     // function that returns the image files in the system
     public static List<File> getImageFiles(){return imageFiles;}
-    // Command Getters
+    // Command Getters that return the constants for the commands the game master can send to game players
     public static String getMusicNextCommand(){return MUSIC_NEXT_COMMAND;}
     public static String getMusicPreviousCommand(){return MUSIC_PREVIOUS_COMMAND;}
     public static String getMusicToggleCommand(){return MUSIC_TOGGLE_COMMAND;}
